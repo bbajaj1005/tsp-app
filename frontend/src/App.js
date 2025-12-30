13308f87a1bdfd51e87fa13c085c8763af44801a
kapil.dev@bhrigu.tech <kapil.dev@bhrigu.tech>
2025-12-30 13:33:44 +0530

    updates

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Task Manager - Azure 3-Tier App</h1>
+        <p>Frontend application is running.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
