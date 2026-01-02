92a0a3302cbd215d7a4f7e18bb187c4170169f9e
vaibhavwagh9119 <vaibhavwagh9119@gmail.com>
2026-01-02 15:36:21 +0530

    remove addon from aks for law

--- terraform/main.tf
+++ terraform/main.tf
@@ -134,9 +134,7 @@ resource "azurerm_kubernetes_cluster" "main" {
     service_cidr   = "10.1.0.0/16"
     dns_service_ip = "10.1.0.10"
   }
-    oms_agent {
-    log_analytics_workspace_id = azurerm_log_analytics_workspace.main.id
-  }
+  
 }
 
 # App Service Plan
