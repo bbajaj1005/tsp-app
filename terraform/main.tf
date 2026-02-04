26b62cf0fc21916b3b7175802de02c6e04a8b482
bbajaj1005 <bhawna.bajaj@rsystems.com>
2026-02-04 15:08:07 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -167,7 +167,7 @@ resource "azurerm_service_plan" "main" {
   resource_group_name = azurerm_resource_group.main.name
   location            = azurerm_resource_group.main.location
   os_type             = "Linux"
-  sku_name            = "F1"
+  sku_name            = "P1v2"
 }
 
 
