f4fce4ab7aef21c5f33d0031049b8dd91f6bd6c0
bbajaj1005 <bhawna.bajaj@rsystems.com>
2026-02-04 15:15:24 +0530

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,8 +1,8 @@
 
 terraform {
   backend "azurerm" {
-    resource_group_name  = "POC-Kapil"         # Replace with your RG name
-    storage_account_name = "tspsa01567rytg"    # Your storage account name
+    resource_group_name  = "avnet-backend"         # Replace with your RG name
+    storage_account_name = "saavnetbackend"    # Your storage account name
     container_name       = "infra"             # The blob container for state
     key                  = "terraform.tfstate" # Path/key for the state file
   }
