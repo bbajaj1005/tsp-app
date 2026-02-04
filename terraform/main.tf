84ee7d716a3fdf2e89453eb18cd4cc0db20967c5
bbajaj1005 <bhawna.bajaj@rsystems.com>
2026-02-04 13:42:58 +0530

    Update main.tf
    Updating the sku_name from P1v2 to S1 then S1 to B1 and now finally from B1 to P0v3

--- terraform/main.tf
+++ terraform/main.tf
@@ -161,13 +161,13 @@ resource "azurerm_kubernetes_cluster" "main" {
 }
 
 # App Service Plan
-## Updating the sku_nme from P1v2 to B1 
+## Updating the sku_nme from P1v2 to P0v3 
 resource "azurerm_service_plan" "main" {
   name                = "asp-tsp-${var.environment}"
   resource_group_name = azurerm_resource_group.main.name
   location            = azurerm_resource_group.main.location
   os_type             = "Linux"
-  sku_name            = "B1"
+  sku_name            = "P0v3"
 }
 
 
